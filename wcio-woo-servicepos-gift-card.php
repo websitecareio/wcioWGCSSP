0ce1c986a1abde4337bc0b82298948c83d4c63b4
Kim Vinberg <support@websitecare.io>
2023-08-30 00:17:26 +0200

    Update wcio-woo-servicepos-gift-card.php

--- wcio-woo-servicepos-gift-card.php
+++ wcio-woo-servicepos-gift-card.php
@@ -3,7 +3,7 @@
  * Plugin Name: Woo Gift Cards synchronize Customers 1st. (Formerly known as ServicePOS)
  * Plugin URI: https://websitecare.dk/
  * Description: Synchronize WooCommerce gift cards with Customers 1st. (Formerly known as ServicePOS)
- * Version: 1.3.8
+ * Version: 1.3.9
  * Author: Websitecare.dk
  * Author URI: https://websitecare.dk
  */
